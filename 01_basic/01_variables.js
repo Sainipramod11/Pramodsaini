d591821066e4d880021a4fe60a1df6086fd03923
Sainipramod11 <pramodkuma2524@gmail.com>
2024-05-23 10:28:53 +0000

    datatypes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,15 @@
+const account_Id = 7206514288
+let account_Email = "Pom@gmail.com"
+var account_Password ="12345"
+account_City = "Palwal"
+let account_State
+
+// account_Id = 2 // not allowed
+
+acoount_Email = "pomy@gmail.com"
+account_Password = "4321"
+account_City = "north"
+
+console.log(account_Id);
+
+console.table([account_Id,account_Email,account_Password,account_City,account_State])
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) // we are using node.js, not browser
+
+console.log(3+3)
+
+console.log("pramod")
+
+let name = "pramod"
+let age = 22
+let isloggedin = false
+let state = null
+
+// numbr => 2 to power 53
+// bigint 
+
+// string => ""
+// boolen => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+console.log(typeof undefined);// undefined
+console.log(typeof null);// object
